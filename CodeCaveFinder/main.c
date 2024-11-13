25e91482edda3533685a12570ec486b853d8f54c
Tajus-io <ftajustf@gmail.com>
2024-11-13 21:12:10 +0200

    Fix linting and code formatting

--- CodeCaveFinder/main.c
+++ CodeCaveFinder/main.c
@@ -2,15 +2,7 @@
 #include <stdio.h>
 #include <winternl.h>
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #pragma comment(lib, "ntdll.lib")
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #define CAVE_SIZE 50
 #define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
@@ -27,20 +19,16 @@ typedef NTSTATUS(NTAPI* _NtReadFile)(
     PULONG Key
 );
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int FindCodeCave(char* target) {
     IO_STATUS_BLOCK iosb;
     HANDLE file;
     
-    file = CreateFileA(target, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    file = CreateFileA(target, GENERIC_READ, 0, NULL, OPEN_EXISTING, 
+                      FILE_ATTRIBUTE_NORMAL, NULL);
     if (file == INVALID_HANDLE_VALUE) {
         printf("[!] Failed to open file\n");
         return 0;
     }
-
-
 
     DWORD fsize = GetFileSize(file, NULL);
     BYTE* buffer = (BYTE*)malloc(fsize);
@@ -50,7 +38,10 @@ int FindCodeCave(char* target) {
         return 0;
     }
 
-    _NtReadFile NtReadFile = (_NtReadFile)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtReadFile");
+    _NtReadFile* NtReadFile = (_NtReadFile*)GetProcAddress(
+        GetModuleHandleA("ntdll.dll"), 
+        "NtReadFile"
+    );
     if (!NtReadFile) {
         printf("[!] Failed to get NtReadFile\n");
         free(buffer);
@@ -58,11 +49,9 @@ int FindCodeCave(char* target) {
         return 0;
     }
 
-
-
-    NTSTATUS status = NtReadFile(file, NULL, NULL, NULL, &iosb, buffer, fsize, NULL, NULL);
+    NTSTATUS status = NtReadFile(file, NULL, NULL, NULL, &iosb, 
+                                buffer, fsize, NULL, NULL);
     CloseHandle(file);
-
 
     if (status != STATUS_SUCCESS) {
         printf("[!] Failed to read file\n");
@@ -70,12 +59,9 @@ int FindCodeCave(char* target) {
         return 0;
     }
 
-
     PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)buffer;
     PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)(buffer + dos->e_lfanew);
     PIMAGE_SECTION_HEADER first = IMAGE_FIRST_SECTION(nt);
-
-
 
     for (int i = 0; i < nt->FileHeader.NumberOfSections; i++) {
         BYTE* start = buffer + first[i].PointerToRawData;
@@ -98,24 +84,24 @@ int FindCodeCave(char* target) {
                 check_size:            ; continue
             }
 
-
             if (nullcount >= CAVE_SIZE) {
                 printf("[+] Cave in section %s at RVA: 0x%x with size: %d bytes\n",
-                    first[i].Name, first[i].VirtualAddress + x - nullcount + 1, nullcount);
+                       first[i].Name, 
+                       first[i].VirtualAddress + x - nullcount + 1, 
+                       nullcount);
                 total_caves++;
                 nullcount = 0;
             }
         }
         if (total_caves > 0) {
-            printf("\n[*] Found %d caves in section %s\n", total_caves, first[i].Name);
+            printf("\n[*] Found %d caves in section %s\n", 
+                   total_caves, first[i].Name);
         }
     }
 
     free(buffer);
     return 1;
 }
-
-
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
